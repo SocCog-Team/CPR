c02f5944185e33a77a0b74960301e403348aa58f
FelixSchneider1990 <66476637+FelixSchneider1990@users.noreply.github.com>
2021-10-12 10:00:50 +0200

    New function added

---
+++ Matlab/CPR_target_reward.m
@@ -0,0 +1,21 @@
+function out = CPR_target_reward(t)
+
+out     = [];
+
+for i = 1:size(t.trg_ts,1)
+    for iTrg = 1:length(t.trg_ts{i})
+        [~,idx]     = min(abs(t.js_ts{i} - t.trg_ts{i}(iTrg)));
+        str         = t.js_str{i}(idx);
+        acc         = abs(1 - abs(t.rdp_dir(i) - t.js_dir{i}(idx)) / 180);
+  
+        trg(1)      = str * acc;
+        trg(2)      = t.ss_coh(i);
+        trg(3)      = t.trg_hit{i}(iTrg);
+        out         = [out; trg];
+    end
+end
+
+idx     = logical(out(:,3));
+vs      = violinplot(out(idx,1),out(idx,2));
+
+end
