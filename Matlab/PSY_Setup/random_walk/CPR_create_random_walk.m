bc86da9735295804597923930ebd0fd68e617e1e
FelixSchneider1990 <66476637+FelixSchneider1990@users.noreply.github.com>
2024-05-14 11:05:42 +0200

    update

--- Matlab/PSY_Setup/random_walk/CPR_create_random_walk.m
+++ Matlab/PSY_Setup/random_walk/CPR_create_random_walk.m
@@ -7,10 +7,10 @@ if nargin < 1
     param.walk_duration_ms      	= 60000;                % Duration of random walk (stimulus cycle)
     param.Fs                        = 1000 / 120;           % Screen sampling rate
     param.snr_list                  = [.2 .4 .6 .8];        % Stimulus coherence
-    param.polar_step_size           = 0.05;                  % Step size in polar space
-    param.reward_probability        = 0.05;                 % Probability of reward target appearance
-    param.feedback_duration_ms      = param.Fs * 30;        % Duration of feedback presentation
-    param.feedback_interval_ms      = param.Fs * 60;        % Interval between feedback presentation
+    param.polar_step_size           = 0.05;                 % Step size in polar space
+    param.reward_probability        = 0.0025;                % Probability of reward target appearance
+    param.feedback_duration_ms      = param.Fs * 6;         % Duration of target presentation
+    param.feedback_interval_ms      = param.Fs * 60;        % Interval between target presentations
 end
 
 % Initialize variables
@@ -35,13 +35,16 @@ end
 
 % Perform random walk in polar space
 for iSample = 2:nSamples*2 % Generate double the amount of samples to be on the safe side    
+    % Reset flag
+    feedback_flag                   = false;
     
     % Update stimulus direction with random step from noise distribution: mean=0; std=param.polar_step_size
     out.RDP_direction(iSample)  	= out.RDP_direction(iSample-1) + (randn * param.polar_step_size);
       
     % Check for reward target appearance: Roll dice but only accept after certain time interval
-    if rand < param.reward_probability && (iSample-out.feedback_ts(end)) > param.feedback_interval_ms
+    if rand < param.reward_probability && (iSample-out.feedback_ts(end)) > (param.feedback_interval_ms/param.Fs)
         cnt                         = cnt+1;
+        feedback_flag              	= true;
         out.feedback_ts(cnt)        = iSample; % Frame of feedback presentation
            
         % Extract direction values between the end of ITI (last target) and current target
@@ -56,14 +59,14 @@ for iSample = 2:nSamples*2 % Generate double the amount of samples to be on the 
     end
     
     %%% Keep RDP direction stable after target for target presentation time
-    if (iSample-out.feedback_ts(end)) < (param.feedback_duration_ms/param.Fs)
-        out.RDP_direction(iSample)	= out.RDP_direction(iSample-1); % Fix stimulus direction for duration of feedback
+    if ~feedback_flag && (iSample-out.feedback_ts(end)) < (param.feedback_duration_ms/param.Fs)
+        out.RDP_direction(iSample)      = out.RDP_direction(iSample-1); % Fix stimulus direction for duration of feedback
     end
 end
 
 % Convert to degree
 out.RDP_direction                   = mod(out.RDP_direction, 2 * pi); % Wrap around if exceeding 2*pi
-out.RDP_direction                   = round(rad2deg(out.RDP_direction));
+out.RDP_direction                   = rad2deg(out.RDP_direction);
 
 end
 
