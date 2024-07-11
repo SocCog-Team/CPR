4c3c70ca12f8dbff9589f104dbda1d92e9e4ff2a
FelixSchneider1990 <66476637+FelixSchneider1990@users.noreply.github.com>
2024-07-11 14:05:03 +0200

    update

--- Matlab/PSY_Setup/random_walk/check_bonus_payment.m
+++ Matlab/PSY_Setup/random_walk/check_bonus_payment.m
@@ -1,9 +1,10 @@
 % Connect to PSY4, then change into data folder
 addpath '/Users/fschneider/Documents/GitHub/CPR/Matlab/preprocessing'
-cd /Volumes/cnl/Documents/MWorks/Data
+% cd /Volumes/cnl/Documents/MWorks/Data
+cd /Volumes/cnl-1/Documents/MWorks/Data
 
 % Import file
-d = CPR_import_mwk2('20240613_kasanm_CPRcompetition_block4.mwk2', {'INFO_'}, false);
+d = CPR_import_mwk2('20240708_madanm_CPRcooperation_block5.mwk2', {'INFO_'}, false);
 
 % P1 bonus
 tmp = d.value(d.event == 'INFO_bonus_ply1_cents');
